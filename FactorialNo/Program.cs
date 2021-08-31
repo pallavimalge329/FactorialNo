f2a29545fd12a9e97e368314029e1727c7528415
HP <HP@DESKTOP-N5QBMVO>
2021-08-31 14:16:26 +0530

    Add project files.

---
+++ FactorialNo/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FactorialNo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
